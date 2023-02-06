a657bd656581964ddafc74c65ebd9ba0dfa96943
Bruno Milhomem <bvmilhomem@gmail.com>
2023-02-06 13:05:23 -0500

    Formatted python code

--- data_storage.py
+++ data_storage.py
@@ -1,21 +1,24 @@
 """ Authenticate with boto3 """
 # Ref: ttps://www.gormanalysis.com/blog/connecting-to-aws-s3-with-python/
-import os
+#import os
 import boto3
-import pandas as pd
+#import pandas as pd
 
-class DataStorage:    
-    """This moduel provides functionality to upload stock data into the Amazon S3 bucket named stock-market-data7
+
+class DataStorage:
+    """This moduel provides functionality to upload stock data
+    into the Amazon S3 bucket named stock-market-data7
     """
     # default constructor
+
     def __init__(self):
         self.s3 = boto3.resource(
-                        service_name='s3',
-                        region_name='ca-central-1',
-                        aws_access_key_id='AKIAQLIFTMJPM5O3Y4DU',
-                        aws_secret_access_key='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'
-                        )
-        
+            service_name='s3',
+            region_name='ca-central-1',
+            aws_access_key_id='AKIAQLIFTMJPM5O3Y4DU',
+            aws_secret_access_key='nsCD0P6rzcgkGLY8jUvwUpKop4zRNl66gh0JhT8W'
+        )
+
     def list_bucket(self):
         """This function is listing the buckets accessbile by your account"""
         print('These are the buckets accessbile by your account:')
@@ -24,8 +27,10 @@ class DataStorage:
 
     def upload_file(self):
         """Read and Write files into S3 bucket"""
-        self.s3.Bucket('stock-market-data7').upload_file(Filename='fetched.csv', Key='fetched.csv')
-        print(f'The file has been uploaded into S3!')
+        self.s3.Bucket(
+            'stock-market-data7').upload_file(Filename='fetched.csv', Key='fetched.csv')
+        print('The file has been uploaded into S3!')
+
 
 ds = DataStorage()
 ds.list_bucket()
