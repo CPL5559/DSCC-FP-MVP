f46b71db9ca3b5ac3eadeca42888d6aeccdd3435
Archies98 <desaiarchies1998@gmail.com>
2023-02-09 18:36:05 -0500

    added config file with credentials; updated data_storage

--- data_storage.py
+++ data_storage.py
@@ -1,36 +1,35 @@
 """ Authenticate with boto3 """
-# Ref: ttps://www.gormanalysis.com/blog/connecting-to-aws-s3-with-python/
-#import os
+import json
 import boto3
-#import pandas as pd
-
-
 class DataStorage:
     """This moduel provides functionality to upload stock data
     into the Amazon S3 bucket named stock-market-data7
     """
-    # default constructor
-
     def __init__(self):
-        self.s3 = boto3.resource(
+        with open("DSCC-FP-MVP-Configuration.json", "r", encoding='utf-8') as jsonfile:
+            data = json.load(jsonfile)
+        self.s3_access_object = boto3.resource(
             service_name='s3',
             region_name='ca-central-1',
-            aws_access_key_id='<add-key-id-here>',
-            aws_secret_access_key='<add-secret-key-here>'
+            aws_access_key_id=data["credentials"]["aws_access_key_id"],
+            aws_secret_access_key=data["credentials"]["aws_secret_access_key"]
         )
 
     def list_bucket(self):
         """This function is listing the buckets accessbile by your account"""
         print('These are the buckets accessbile by your account:')
-        for bucket in self.s3.buckets.all():
+        for bucket in self.s3_access_object.buckets.all():
             print(bucket.name)
 
-    def upload_file(self):
-        """Read and Write files into S3 bucket"""
-        self.s3.Bucket(
-            'stock-market-data7').upload_file(Filename='fetched.csv', Key='fetched.csv')
+    def upload_file(self,bucket_name,filename):
+        """Upload file from local system to the S3 bucket"""
+        self.s3_access_object.Bucket(bucket_name).upload_file(Filename=filename, Key='fetched.csv')
         print('The file has been uploaded into S3!')
 
+    def download_file(self,bucket_name,filename):
+        """Download file from S3 bucket to local file system"""
+        self.s3_access_object.Bucket(bucket_name).download_file(filename, 'fetched.csv')
+        print('The file has been downloaded from S3!')
 
 ds = DataStorage()
-ds.list_bucket()
+ds.download_file('stock-market-data7','fetched.csv')
