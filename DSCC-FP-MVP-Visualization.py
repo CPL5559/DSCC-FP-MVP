02126d0e73e8541ea3dabb943a6134b3cf34b8a9
Archies98 <desaiarchies1998@gmail.com>
2023-02-15 19:18:45 -0500

    updated the visualization file

--- DSCC-FP-MVP-Visualization.py
+++ DSCC-FP-MVP-Visualization.py
@@ -1,32 +1,27 @@
-# Importing all the necessary libraries
-import pandas as pd 
-import warnings
-warnings.filterwarnings("ignore")
-import plotly
-import plotly.graph_objs as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, iplot, plot
+"""This module contains the visualization class"""
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-class MVP_Visualizaton:
-    '''This class will helps, interact with, and better understand data. Whether simple or complex, the right visualization'''
-    def linemap(self,df,col,col1):
-        ''' The function is used to plot the line map when we pass dataframe and two labels from dataframe '''
-        sns.lineplot(x=df[col], y =df[col1])
+class Visualizaton:
+    '''This class wraps seaborn functions into simple class methods'''
+    def linemap(self,dataframe,col,col1):
+        ''' The function makes line plots when we pass dataframe and two labels from dataframe'''
+        sns.lineplot(x=dataframe[col], y =dataframe[col1])
         plt.show()
 
-    def boxplot(self,df,col):
-        ''' The function is used to plot the box when we pass dataframe and one label from dataframe '''
+    def boxplot(self,dataframe,col):
+        ''' The function makes a boxplot when we pass dataframe and one label from dataframe '''
         sns.set_style('whitegrid')
-        sns.boxplot(x=df[col])
+        sns.boxplot(x=dataframe[col])
         plt.show()
 
-    def heatmap(self,df):
-        ''' The function is used to plot the heatmap when we pass dataframe to show correlation among the labels in dataframe '''
-        # df = df.drop(columns =['Date'])
-        sns.heatmap(df.corr(), annot=True, linewidth=.5)
+    def heatmap(self,dataframe):
+        ''' The function makes a heatmap when we pass dataframe
+          to show correlation among all the numerical columns in dataframe'''
+        sns.heatmap(dataframe.corr(), annot=True, linewidth=.5)
         plt.show()
 
 df = pd.read_csv("fetched.csv")
-visualization = MVP_Visualizaton()
+visualization = Visualizaton()
 visualization.heatmap(df)
