d19a2158dfd6b01aaaab121fb53de1df6beaf979
bmilhomem <bruno.milhomem@calix.com>
2023-03-06 10:48:28 -0500

    Structure of the file

--- DSCC-FC-MVP-Timeseries-Analysis.py
+++ DSCC-FC-MVP-Timeseries-Analysis.py
@@ -9,3 +9,11 @@ from pandas import Series           # To work on series
 import warnings                     # To ignore the warnings
 warnings.filterwarnings("ignore")
 
+class Timeseries:
+    """Display the required time-series analysis"""
+    def timeseries_analysis(self):
+        """Time-series visualization"""
+        # Apply logic here
+
+df = pd.read_csv("fetched.csv")
+ts = Timeseries()
